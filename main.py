63e3a2b71d3bd6d5a7ba3cf81bd098cdc59ca0f2
Kristian Pindstrup <kpind@aarhus.dk>
2021-09-22 13:03:48 +0200

    updated xml and main

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from pathlib import Path
 import xml.etree.ElementTree as ET
-import datetime
 
+
+index_import = r'D:\contextupdater\xml_samples\contextDocumentationIndex.xml'
+sampleb = r'xml_samples/beginindex.xml'
+samplee = r'xml_samples/endindex.xml'
+
+tree = ET.parse(samplee)
+root = tree.getroot()
+
+print(ET.tostring(root, encoding='UTF-8', method="xml").decode('UTF-8'))
